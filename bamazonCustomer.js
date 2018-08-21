fb1d206df6871de183795c336f9582e85c232163
byt3_h34d <mr.tybopp@gmail.com>
2018-08-21 16:26:41 -0500

    building runSale() function

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,6 @@
+// Initializing global variables
 const mysql = require("mysql");
-// const inquirer = require("inquirer");
-
+const inquirer = require("inquirer");
 const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -8,12 +8,14 @@ const connection = mysql.createConnection({
     password: "PASSWORD",
     database: "bamazon"
   });
-  
+
+  // Initial function to run upon app execution
   connection.connect(function(err) {
     if (err) throw err;
-   showProducts();
+   runSale();
   });
 
+  // Function to display all objects
   function showProducts() {
     const query = "SELECT * FROM products";
     connection.query(query, function(err, res) {
@@ -22,9 +24,57 @@ const connection = mysql.createConnection({
             console.log(
                 "\nItem ID: " + res[i].item_id + 
                 "\nProduct Name: " + res[i].product_name + 
-                "\nDepartment Name: " + res[i].department_name + 
                 "\nPrice: " + "$" + res[i].price + "\n*~*~*~*~*~*~*~\n")
           }
     })
   };
 
+  function runSale() {
+    // Displays all products in the store  
+    showProducts();
+    console.log("WELCOME TO BAMAZON!")
+    inquirer.prompt([
+        {
+          name: "item",
+          type: "input",
+          message: "Enter an Item ID to begin your purchase: ",
+          validate: function(value) {
+            if (isNaN(value) === false) {
+              return true;
+            }
+            return false;
+          }
+        },
+        {
+          name: "quantity",
+          type: "input",
+          message: "Enter an item quantity: ",
+          validate: function(value) {
+            if (isNaN(value) === false) {
+              return true;
+            }
+            return false;
+          }
+        }
+      ])
+      .then(function(answer) {
+        const queryItem = "SELECT item_id, product_name, price, stock_quantity FROM products WHERE item_id=?";
+        
+        connection.query(queryItem, answer.item, function(err, res) {
+            if (err) throw err;
+          for (var i = 0; i < res.length; i++) {
+            console.log(
+              "\nItem ID: " +
+                res[i].item_id +
+                "\nProduct Name: " +
+                res[i].product_name +
+                " || Artist: " +
+                res[i].artist +
+                " || Year: " +
+                res[i].year
+            );
+          }
+          ();
+        });
+      });
+  }
